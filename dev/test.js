f0d3f5a3d29646ed21424ca1903dfe2bb765099a
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-02 17:02:39 +0530

    test genesis

--- dev/test.js
+++ dev/test.js
@@ -2,19 +2,4 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-const previousBlockHash = 'UjwalUdai'
-
-const currentBlockData = [
-    {
-        amount: 100,
-        sender: 'ujwal',
-        recipient: 'udai',
-    },
-    {
-        amount: 1000,
-        sender: 'udai',
-        recipient: 'ujwal',
-    },
-]
-
-console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+console.log(bitcoin);
