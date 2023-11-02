4dc53b9a1cb79c16c6497dd4e84a24ad817e9113
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-02 15:24:03 +0530

    Pending Transactions

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,26 +1,38 @@
 function Blockchain() {
     this.chain = [];
-    this.newTrasactions = [];
+    this.pendingTrasactions = [];
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        trasactions: this.newTrasactions,
+        trasactions: this.pendingTrasactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash,
     };
 
-    this.newTrasactions = [];
+    this.pendingTrasactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
 }
 
 Blockchain.prototype.getLastBlock = function() {
-    return this.chain[this.chain.length - 1]
+    return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient,
+    };
+
+    this.pendingTrasactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1
 }
 
 
