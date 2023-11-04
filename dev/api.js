d9014ac45d203a80578f3ce0792e68772183c0c4
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-04 15:17:41 +0530

    install uuid

--- dev/api.js
+++ dev/api.js
@@ -21,13 +21,23 @@ app.get('/blockchain', (req, res) => {
 
 app.post('/transaction', (req, res) => {
     const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
-    res.json({ note: `Transaction will be added in a block ${blockIndex} .`})
+    res.json({ note: `Transaction will be added in a block ${blockIndex} .`});
 });
 
 app.get('/mine', (req, res) => {
-    const lastBlock = bitcoin.
-    const newBlock = bitcoin.createNewBlock()
-    res.send("Hi listening already dw.");
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transaction: bitcoin.pendingTrasactions,
+        index: lastBlock['index'] + 1,
+    };
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash)
+    res.json({
+        note: "New Block mined Successfully",
+        block: newBlock
+    });
 });
 
 app.listen(3000, () => {
