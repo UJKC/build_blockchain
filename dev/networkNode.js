5e0b20a840763708a54ae4a72290383cff019d94
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-05 21:55:04 +0530

    register and broadcast node part 1

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -15,6 +15,8 @@ const bitcoin = new Blockchain();
 // Make it access package.json start file
 const port = process.argv[2]
 
+const rp = require('request-promise');
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
@@ -54,7 +56,52 @@ app.post('/register-and-broadcast-node', (req, res) => {
     if (bitcoin.networkNodes.indexOf(newNodeUrl) == -1) {
         bitcoin.networkNodes.push(newNodeUrl);
     }
+    const registerNodePromises = [];
+    bitcoin.networkNodes.forEach(newNodeUrl => {
+        const requestOption = {
+            uri: newNodeUrl + '/register-node',
+            method: 'POST',
+            body: { 
+                newNodeUrl: newNodeUrl,
+                json: true
+            }
+        };
+        registerNodePromises.push(rp(requestOption));
+    });
+    Promise.all(registerNodePromises)
+    .then(data =>{
+
+    });
 });
+
+/*
+Sure, I'll explain the code block you provided step by step:
+
+1. **Endpoint Definition**:
+   - This code block defines an HTTP POST endpoint at `/register-and-broadcast-node`. This endpoint is where a new node will register with the existing nodes in the network.
+
+2. **New Node URL Extraction**:
+   - The `newNodeUrl` is extracted from the request body using `req.body.newNodeUrl`. This assumes that the incoming request contains a JSON body with a `newNodeUrl` property.
+
+3. **Network Node Registration**:
+   - It checks if the `newNodeUrl` is not already in the `bitcoin.networkNodes` array. This array is used to keep track of all nodes in the network.
+   - If the `newNodeUrl` is not found in the array (i.e., its index is -1), it adds the `newNodeUrl` to the `bitcoin.networkNodes` array. This step registers the new node locally in this node's list of network nodes.
+
+4. **Registering the New Node with Other Nodes**:
+   - It creates an array called `registerNodePromises` to store promises returned by asynchronous HTTP requests to register the new node with the existing nodes in the network.
+   - It iterates over the `bitcoin.networkNodes` array (which should now include the newly added node) and makes a POST request to each existing node.
+   - For each existing node, it constructs the request options using the `rp` library (assuming `rp` is `request-promise`).
+   - The request options include:
+     - The URI, which is the URL of the existing node plus `/register-node`. This endpoint is expected to handle the registration process on the receiving node.
+     - The HTTP method is set to POST.
+     - The request body contains the `newNodeUrl` property, and `json: true` is specified to indicate that the body should be sent as JSON.
+
+5. **Promise.all**:
+   - It uses `Promise.all` to wait for all registration requests to complete. The `registerNodePromises` array contains promises for all registration requests.
+   - When all promises are resolved, the `then` block is executed. However, in the provided code, the `then` block is empty (`then(data => { })`). You would typically handle the responses from the other nodes in this block, which may involve synchronization, validation, or other blockchain-related tasks.
+
+This code block is part of a blockchain network and is responsible for registering a new node with the existing nodes in the network, facilitating the decentralization of the blockchain. The new node is added to the local node's list of network nodes, and registration requests are sent to the existing nodes to let them know about the new addition. The actual logic for processing these requests and updating the network state would be implemented in the `/register-node` endpoint on each existing node.
+*/
 
 app.post('/register-node', (req, res) => {
     const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
